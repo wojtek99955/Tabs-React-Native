58bc767caba6dbf57af76e6a7bd65406bd8d7b92
Wojtek Książek <wojtek99955@gmail.com>
2022-05-24 15:40:04 +0200

    styles update

--- About.js
+++ About.js
@@ -1,14 +1,25 @@
 import { View, Text, SafeAreaView } from "react-native";
+import styled from "styled-components";
+
+const Title = styled.Text`
+  text-align: center;
+  font-size: 30px;
+  margin-top: 10px;
+`;
+const Description = styled.Text`
+  font-size: 15px;
+  margin: 15px 15px;
+`;
 
 function About() {
   return (
     <SafeAreaView>
-      <Text>About</Text>
-      <Text>
+      <Title>About</Title>
+      <Description>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima minus
         aspernatur nobis sequi officiis dolorem, adipisci, tempore similique
         qui, voluptatem beatae eos reiciendis cupiditate iusto!
-      </Text>
+      </Description>
     </SafeAreaView>
   );
 }
