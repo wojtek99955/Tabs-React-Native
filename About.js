60044bd61d045b2113a281f4e66eb14bbd1b7869
Wojtek Książek <wojtek99955@gmail.com>
2022-05-24 14:24:06 +0200

    About added

---
+++ About.js
@@ -0,0 +1,16 @@
+import { View, Text, SafeAreaView } from "react-native";
+
+function About() {
+  return (
+    <SafeAreaView>
+      <Text>About</Text>
+      <Text>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima minus
+        aspernatur nobis sequi officiis dolorem, adipisci, tempore similique
+        qui, voluptatem beatae eos reiciendis cupiditate iusto!
+      </Text>
+    </SafeAreaView>
+  );
+}
+
+export default About;
