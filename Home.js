5431b5a862b42cc8c2d820f08073dcb3c076e0b2
Wojtek Książek <wojtek99955@gmail.com>
2022-05-24 14:23:12 +0200

    Home added

---
+++ Home.js
@@ -0,0 +1,11 @@
+import { View, Text, SafeAreaView } from "react-native";
+
+function Home() {
+  return (
+    <SafeAreaView>
+      <Text>Home</Text>
+    </SafeAreaView>
+  );
+}
+
+export default Home;
