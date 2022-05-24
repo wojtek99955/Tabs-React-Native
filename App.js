e81c12dcac2a7b02b3ecf1f78be89b6239a6afde
Wojtek Książek <wojtek99955@gmail.com>
2022-05-24 14:58:01 +0200

    Tabs added

--- App.js
+++ App.js
@@ -1,12 +1,19 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { NavigationContainer } from "@react-navigation/native";
+import Home from "./Home";
+import About from "./About";
 
 export default function App() {
+  const Tabs = createBottomTabNavigator();
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Tabs.Navigator>
+        <Tabs.Screen name="Home" component={Home} />
+        <Tabs.Screen name="About" component={About} />
+      </Tabs.Navigator>
+    </NavigationContainer>
   );
 }
 
